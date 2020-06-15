35f3345a8e0cc1aea36e645cb08d5bf15b7c6b79
Blackwell <61912692+JoBlackwell@users.noreply.github.com>
2020-06-15 01:05:24 -0600

    Recreating game_manager.js work
    Recreating previous work that was not allowed in a pull request, giving the following error: 'this page out of date. start over.' Reaccomplished work consists of drawOnce function() for leaderboard.

--- docs/scripts/game_manager.js
+++ docs/scripts/game_manager.js
@@ -99,6 +99,11 @@ game.gameController = {
         for (var i = 0; i < game.controls.length; i++) {
             if (engine.input.pressed(game.controls[i])) {
 				// Update game state to Start Scene
+		    //Reset player object
+		game.player.reset();
+		    //Reset leaderboard table
+		game.top10players.hideTable();
+		    //Update game state to Start Scene
                 game.currState = game.gameState[0];
 				// Hide all elements
                 game.hideElements.hideAll();
@@ -205,9 +210,16 @@ game.drawOnce = function () {
             break;
         case 'leaderboard':
             // Draw images on the canvas
-            
+            this.leaderboardBackground.draw();
+	    this.leaderboardPlayerScore.draw();
+	    this.leaderboardSponsorBox.draw();
+	    this.leaderboardSponsorLogo.draw();
+	    this.finalPlayerScore.draw();
+            this.top10players.adjustStyle();
+		    
             // Display buttons
-            
+            this.menuButton.adjustStyle();
+	    this.leaderboardRetryButton.adjustStyle();
             break;
         default:
             break;
